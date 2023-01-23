08053cf09873a2bcd62bf68f27b5cdc4f2106bdd
QALightLecorPython <denkondrat13+1@gmail.com>
2023-01-23 21:38:33 +0200

    [Lesson 17] Add additional design patterns: - Add log decorator based on docstings - Add value objects for Post and User - Add factory for browser creation - Add support for Firefox along with Chrome

--- pages/start_page.py
+++ pages/start_page.py
@@ -1,8 +1,6 @@
-import logging
-
 from constants.start_page import StartPageConst
 from pages.base_page import BasePage
-from pages.utils import wait_until_ok
+from pages.utils import wait_until_ok, log_wrapper
 
 
 class StartPage(BasePage):
@@ -11,32 +9,34 @@ class StartPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.const = StartPageConst
-        self.log = logging.getLogger("[StartPage]")
 
-    def sign_in(self, username, password):
+    @log_wrapper
+    def sign_in(self, user):
         """Sign in using provided values"""
         # Fill fields
-        self.fill_field(xpath=self.const.SIGN_IN_USERNAME_FIELD_XPATH, value=username)
-        self.fill_field(xpath=self.const.SIGN_IN_PASSWORD_FIELD_XPATH, value=password)
+        self.fill_field(xpath=self.const.SIGN_IN_USERNAME_FIELD_XPATH, value=user.username)
+        self.fill_field(xpath=self.const.SIGN_IN_PASSWORD_FIELD_XPATH, value=user.password)
         # Click on SignIn button
         self.click(self.const.SIGN_IN_BUTTON_XPATH)
 
+    @log_wrapper
     def verify_sign_in_error(self):
         """Verify that text is matches to expected"""
         assert self.compare_element_text(xpath=self.const.SIGN_IN_ERROR_XPATH, text=self.const.SIGN_IN_ERROR_TEXT)
 
-    @wait_until_ok(timeout=3, period=0.5)
+    @wait_until_ok(timeout=10, period=1)
     def click_and_validate_sign_up(self):
         """Click on Sign Up button until it disappear"""
         self.click(self.const.SIGN_UP_BUTTON_XPATH)
         assert not self.is_element_exists(self.const.SIGN_UP_BUTTON_XPATH), "Sign Up button didn't disappear"
 
-    def sign_up(self, username, email, password, verify=True):
+    @log_wrapper
+    def sign_up(self, user, verify=True):
         """Sign up using provided values"""
         # Fill fields
-        self.fill_field(xpath=self.const.SIGN_UP_USERNAME_FIELD_XPATH, value=username)
-        self.fill_field(xpath=self.const.SIGN_UP_EMAIL_FIELD_XPATH, value=email)
-        self.fill_field(xpath=self.const.SIGN_UP_PASSWORD_FIELD_XPATH, value=password)
+        self.fill_field(xpath=self.const.SIGN_UP_USERNAME_FIELD_XPATH, value=user.username)
+        self.fill_field(xpath=self.const.SIGN_UP_EMAIL_FIELD_XPATH, value=user.email)
+        self.fill_field(xpath=self.const.SIGN_UP_PASSWORD_FIELD_XPATH, value=user.password)
         # Click on SignUn button
         if verify:
             self.click_and_validate_sign_up()
--- tests/test_start_page.py
+++ tests/test_start_page.py
@@ -1,15 +1,18 @@
 """Tests related to start page"""
 import logging
 
-from pages.utils import random_num, random_str
+import pytest
+
+from constants.base import BaseConstants
 
 
+@pytest.mark.parametrize("browser", [BaseConstants.CHROME, BaseConstants.FIREFOX])
 class TestStartPage:
     """Stores tests for start page base functionality"""
 
     log = logging.getLogger("[TestStartPage]")
 
-    def test_invalid_login(self, start_page):
+    def test_invalid_login(self, start_page, random_user):
         """
         - Pre-conditions:
             - Open start page
@@ -20,14 +23,12 @@ class TestStartPage:
             - Verify error
         """
         # Login as invalid user
-        start_page.sign_in("test123", "pwd123")
-        self.log.info("Logged in as invalid user")
+        start_page.sign_in(random_user)
 
         # Verify error
         start_page.verify_sign_in_error()
-        self.log.info("Error message was verified")
 
-    def test_empty_login(self, start_page):
+    def test_empty_login(self, start_page, empty_user):
         """
         - Pre-conditions:
             - Open start page
@@ -38,14 +39,12 @@ class TestStartPage:
             - Verify error
         """
         # Login as invalid user
-        start_page.sign_in("", "")
-        self.log.info("Logged in as invalid user")
+        start_page.sign_in(empty_user)
 
         # Verify error
         start_page.verify_sign_in_error()
-        self.log.info("Error message was verified")
 
-    def test_register(self, start_page):
+    def test_register(self, start_page, random_user):
         """
         - Pre-conditions:
             - Open start page
@@ -54,15 +53,8 @@ class TestStartPage:
             - Click on Sign Up button
             - Verify registration is successful
         """
-        # Prepare test data
-        username_value = f"{random_str()}{random_num()}"
-        email_value = f"{username_value}@mail.com"
-        password_value = f"{random_str(6)}{random_num()}"
-
         # Fill email, login and password fields
-        hello_page = start_page.sign_up(username=username_value, email=email_value, password=password_value)
-        self.log.info("User was registered")
+        hello_page = start_page.sign_up(random_user)
 
         # Verify register success
-        hello_page.verify_sign_up_message(username=username_value)
-        self.log.info("Registration for user '%s' was success and verified", username_value)
+        hello_page.verify_sign_up_message(username=random_user.username)
